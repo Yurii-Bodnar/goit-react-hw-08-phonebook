7a657fba68cf4fd5fc207ea10fd378ad0a6a2067
Yurii-Bodnar <106067762+Yurii-Bodnar@users.noreply.github.com>
2022-10-14 15:09:44 +0000

    Deploying to gh-pages from @ Yurii-Bodnar/goit-react-hw-08-phonebook@4c5c699d891e9847bd9cd5349ae47929c17eea1b 🚀

---
+++ static/js/680.c13cde3d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[680],{680:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("h1",{children:"Welcome to page"})}}}]);
+//# sourceMappingURL=680.c13cde3d.chunk.js.map
